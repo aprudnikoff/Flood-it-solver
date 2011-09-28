4b4655d859c8566ebbdccd63e97205d442bd26b9
Andrey Prudnikov <a.prudnikov@gmail.com>
2011-09-29 01:00:01 +0300

    bin

--- src/RTest.java
+++ src/RTest.java
@@ -7,7 +7,6 @@ import com.sun.jna.Pointer;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinUser;
-import com.sun.jna.win32.W32APIFunctionMapper;
 
 
 public class RTest {
